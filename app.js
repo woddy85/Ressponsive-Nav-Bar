f1f3ff7bfc1b952cadcc38b3a56b1049863d2d5f
woddy85 <48095572+woddy85@users.noreply.github.com>
2019-04-11 13:52:13 +0200

    resp navbar finish

---
+++ app.js
@@ -0,0 +1,30 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    
+    burger.addEventListener('click',() => {
+        //Toogle Nav
+        nav.classList.toggle('nav-active');
+
+        //Animated Links
+        navLinks.forEach((link, index) => {
+         if(link.style.animation){
+            link.style.animation = ''
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 +1}s`
+            }
+        });
+        //burger Animation
+        burger.classList.toggle('toggle');
+    });
+}
+
+            //This is a cleaner wahy to do it
+            // const app = ()=>{
+            //     navSlide();
+            //     navSlide();
+            //     navSlide();
+            //     navSlide();
+            // }
+navSlide();

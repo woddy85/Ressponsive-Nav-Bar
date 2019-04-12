5ba8fc444fc47d366ef4dac6439412e4c2534f73
woddy85 <w.y.tvedt@gmailcom>
2019-04-12 23:11:48 +0200

    ade background-position center on the main

--- app.js
+++ app.js
@@ -2,17 +2,17 @@ const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li');
-    
-    burger.addEventListener('click',() => {
+
+    burger.addEventListener('click', () => {
         //Toogle Nav
         nav.classList.toggle('nav-active');
 
         //Animated Links
         navLinks.forEach((link, index) => {
-         if(link.style.animation){
-            link.style.animation = ''
+            if (link.style.animation) {
+                link.style.animation = ''
             } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 +1}s`
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1}s`
             }
         });
         //burger Animation
@@ -20,11 +20,11 @@ const navSlide = () => {
     });
 }
 
-            //This is a cleaner wahy to do it
-            // const app = ()=>{
-            //     navSlide();
-            //     navSlide();
-            //     navSlide();
-            //     navSlide();
-            // }
+//This is a cleaner wahy to do it
+// const app = ()=>{
+//     navSlide();
+//     navSlide();
+//     navSlide();
+//     navSlide();
+// }
 navSlide();
